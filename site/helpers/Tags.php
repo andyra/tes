4f267754966453a85b6b96a11dc4caf06eef2f84
Andy Smith <asmith.esq@gmail.com>
2018-12-02 13:06:04 -0600

    Add sprite helper

---
+++ site/helpers/Tags.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Statamic\SiteHelpers;
+
+use Statamic\Extend\Tags as AbstractTags;
+
+class Tags extends AbstractTags {
+  public function sprite() {
+    $id = $this->getParam('id');
+    $w = $this->getParam('width') ?: "16";
+    $h = $this->getParam('height') ?: "16";
+    $class = $this->getParam('class') ?: "";
+
+    $sprite = "<svg class='sprite sprite--{$id} {$class}' viewBox='0 0 {$w} {$h}' preserveAspectRatio='xMinYMin meet'>";
+    $sprite .= "<use xlink:href='/site/themes/tes/assets/images/sprite.svg#{$id}'></use>";
+    $sprite .= "</svg>";
+
+    return $sprite;
+  }
+}
