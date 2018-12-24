6cf96a089cb12fc8a17b9ab387381549faad2073
Andy Smith <asmith.esq@gmail.com>
2018-12-24 15:32:25 -0600

    A buncha styles

--- site/helpers/Tags.php
+++ site/helpers/Tags.php
@@ -5,16 +5,16 @@ namespace Statamic\SiteHelpers;
 use Statamic\Extend\Tags as AbstractTags;
 
 class Tags extends AbstractTags {
-  public function sprite() {
-    $id = $this->getParam('id');
-    $w = $this->getParam('width') ?: "16";
-    $h = $this->getParam('height') ?: "16";
-    $class = $this->getParam('class') ?: "";
+	public function sprite() {
+	$id = $this->getParam('id');
+	$w = $this->getParam('width') ?: "16";
+	$h = $this->getParam('height') ?: "16";
+	$class = $this->getParam('class') ?: "";
 
-    $sprite = "<svg class='sprite sprite--{$id} {$class}' viewBox='0 0 {$w} {$h}' preserveAspectRatio='xMinYMin meet'>";
-    $sprite .= "<use xlink:href='/site/themes/tes/assets/images/sprite.svg#{$id}'></use>";
-    $sprite .= "</svg>";
+	$sprite = "<svg class='sprite sprite--{$id} {$class}' viewBox='0 0 {$w} {$h}' preserveAspectRatio='xMinYMin meet'>";
+	$sprite .= "<use xlink:href='/site/themes/tes/assets/images/sprite.svg#{$id}'></use>";
+	$sprite .= "</svg>";
 
-    return $sprite;
-  }
+	return $sprite;
+	}
 }
