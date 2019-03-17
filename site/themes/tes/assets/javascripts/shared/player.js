3ad86a336b8fb4effd378fd247e41f72480130b1
Andy Smith <asmith.esq@gmail.com>
2019-03-17 11:33:18 -0500

    Add track durations

--- site/themes/tes/assets/javascripts/shared/player.js
+++ site/themes/tes/assets/javascripts/shared/player.js
@@ -52,7 +52,7 @@
             html5: true, // Force to HTML5 so that the audio can stream in (best for large files).
             onplay: function() {
               // Display the duration.
-              duration.innerHTML = self.formatTime(Math.round(sound.duration()));
+              duration.innerHTML = formatTime(Math.round(sound.duration()));
 
               // Start upating the progress of the track.
               requestAnimationFrame(self.step.bind(self));
@@ -213,7 +213,7 @@
 
         // Determine our current seek position.
         var seek = sound.seek() || 0;
-        timer.innerHTML = self.formatTime(Math.round(seek));
+        timer.innerHTML = formatTime(Math.round(seek));
         progressBar.style.width = (((seek / sound.duration()) * 100) || 0) + '%';
 
         // If the sound is still playing, continue stepping.
@@ -221,17 +221,17 @@
           requestAnimationFrame(self.step.bind(self));
         }
       },
-
-      // Format the time from seconds to M:SS.
-      // @param  {Number} secs Seconds to format.
-      // @return {String}      Formatted time.
-      formatTime: function(secs) {
-        var minutes = Math.floor(secs / 60) || 0;
-        var seconds = (secs - minutes * 60) || 0;
-
-        return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-      }
     };
+
+    // Format the time from seconds to M:SS.
+    // @param  {Number} secs Seconds to format.
+    // @return {String}      Formatted time.
+    function formatTime(secs) {
+      var minutes = Math.floor(secs / 60) || 0;
+      var seconds = (secs - minutes * 60) || 0;
+
+      return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    }
 
     function seekPercent(e) {
       var p = progress.getBoundingClientRect();
@@ -257,25 +257,26 @@
       return playlist;
     }
 
-    // function tracklistTimes() {
-    //   var jsmediatags = window.jsmediatags;
+    function addTracklistDurations() {
+      Array.prototype.forEach.call(tracklistItems, function(track) {
+        var src = track.getAttribute('data-track');
 
-    //   Array.prototype.forEach.call(tracklistItems, function(el, i) {
-    //     var time = el.querySelector('[datetime]');
-    //     var file = el.getAttribute('data-track');
+        if (src) {
+          console.log("This track should have a duration");
+          var audio = new Audio();
+          var duration = track.querySelector('[data-track-duration]');
+          audio.src = src;
 
-    //     // ID3
-    //     jsmediatags.read(file, {
-    //       onSuccess: function(tag) {
-    //         time.textContent = '000';
-    //         console.log(tag);
-    //       },
-    //       onError: function(error) {
-    //         console.log(error);
-    //       }
-    //     });
-    //   });
-    // }
+          console.log(src);
+          console.log(duration);
+
+          audio.addEventListener('loadedmetadata', function() {
+            var secs = Math.round(audio.duration);
+            duration.textContent = formatTime(secs);
+          });
+        }
+      });
+    }
 
     // Radio
     // -------------------------------------------------------------------------
@@ -372,5 +373,6 @@
         e.stopPropagation();
       }, false);
     }
+    addTracklistDurations();
   }
 })(); // end DOM ready
